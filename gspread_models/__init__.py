d78064e5ea93eb76d1855a94ca38f2fe3191006e
MJ Rossetti <s2t2@users.noreply.github.com>
2024-05-27 15:58:37 -0400

    More direct imports

--- gspread_models/__init__.py
+++ gspread_models/__init__.py
@@ -1,3 +1,14 @@
 
 
 VERSION = "1.0.6"
+
+
+from gspread_models.base import BaseModel
+from gspread_models.service import SpreadsheetService
+from gspread_models.date_parser import DateParser
+
+# declare some classes for higher level import access
+# ... like `from gspread_models import BaseModel`
+# ... instead of `from gspread_models.base import BaseModel`
+# ... see: https://docs.python.org/3/tutorial/modules.html
+__all__ = ['BaseModel', 'SpreadsheetService', 'DateParser']
